d9317bb85db9a518455660d8022eb9ec366a8797
95Stardust <15chriskim@gmail.com>
2023-05-29 09:49:19 -0700

    revert to previous commit(fatal error)

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The sidebar containing the main widget area
  *
@@ -7,11 +8,28 @@
  * @package School_Theme
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+// if (!is_active_sidebar('sidebar-1')) {
+// 	return;
+// }
 ?>
 
+
+// Blog 4 points
+// Create a page called News to display the Blog posts (create at least 3 posts).
+
+// On the News page show the excerpts of the posts instead of the full content and display the sidebar before the footer instead of next to the content.
+
+// Setup Animate on Scroll so each blog post animates into the viewport when scrolling. Include the Animate on Scroll CSS and JS files in your theme instead of using the CDN links. Only enqueue the files for the Post Type of post.
+
+
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+	<?php
+
+	if (is_page()) {
+		dynamic_sidebar('sidebar-1');
+	}
+
+	?>
+
 </aside><!-- #secondary -->
